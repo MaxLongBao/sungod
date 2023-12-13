03256d882a46145b76b8662b17ed2a44a5e303b6
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2023-12-13 18:19:48 +0000

    setup window frame

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import WindowFrame from './WindowFrame';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="app p-3">
+    <div className="app p-5 h-screen">
       <div className="app__close__button flex justify-end">
         <p className="underline cursor-pointer">Close</p>
       </div>
+      <WindowFrame />
     </div>
   );
 }
---
+++ src/WindowFrame.js
@@ -0,0 +1,9 @@
+const WindowFrame = () => {
+  return (
+    <div className="window mt-5">
+      h
+    </div>
+  );
+}
+
+export default WindowFrame;
