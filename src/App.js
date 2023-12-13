eb362438e547a0e42c263ad72d108cf9c4a0a248
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2023-12-13 17:39:38 +0000

    close button

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      
+    <div className="app p-3">
+      <div className="app__close__button flex justify-end">
+        <p className="underline cursor-pointer">Close</p>
+      </div>
     </div>
   );
 }
